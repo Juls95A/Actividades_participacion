cd6703c830cde3db063015168f03b66e4e2296e6
Juliana Alvarez <jalvarez356@soyudemedellin.edu.co>
2023-08-06 20:44:16 -0500

    Programa de pedirle nombre y edad usuario

---
+++ Taller_1/Punto1.py
@@ -0,0 +1,9 @@
+#Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla.
+
+"Hola Usuario, por favor ingrese los siguientes datos: "
+
+nombre = input("Ingrese su nombre")
+edad= input("Ingrese su edad")
+
+print("Te llamas",nombre)
+print("Tienes",edad,"años")
