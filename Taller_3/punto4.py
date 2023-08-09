c3fb1fdf7fd8c3ad7d460035c7320ecdc63e0080
Juliana Alvarez <jalvarez356@soyudemedellin.edu.co>
2023-08-08 21:24:34 -0500

    Ejercicio Concepto POO

--- Taller_3/punto4.py
+++ Taller_3/punto4.py
@@ -1,4 +1,25 @@
 #Cree una clase Rectángulo la cual contiene dos atributos de instancia que representan los puntos que definen sus esquinas. Agregue métodos a la clase Rectángulo para calcular su perímetro, calcular su área e indicar si el rectángulo es un cuadrado.
 
 class Rectangulo:
-    def __init__(self):
+    def __init__(self, ancho, altura):
+        self.ancho = ancho
+        self.altura = altura
+
+    def imprimir_perimetro(self):
+        print("Perímetro:", 2 * (self.ancho + self.altura))
+
+    def imprimir_area(self):
+        print("Área:", self.ancho * self.altura)
+
+    def es_cuadrado(self):
+        if self.ancho == self.altura:
+            print("Es un cuadrado.")
+        else:
+            print("No es un cuadrado.")
+
+mi_rectangulo=Rectangulo()
+
+print(mi_rectangulo.imprimir_perimetro())
+print(mi_rectangulo.imprimir_area())
+
+    
